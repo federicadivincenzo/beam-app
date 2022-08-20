fe8b45c61c60ebf89f360220ed1c797fe422df00
jaxxon alta <jaxxonalta@gmail.com>
2022-08-20 15:38:28 +0200

    small fix

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,19 +1,15 @@
 class ConcertsController < ApplicationController
   before_action :set_conert, only: :show
 
-
   def index
     @concerts = Concert.all.order(created_at: :asc)
   end
 
-  def show
-  end
-
+  def show; end
 
   private
 
-  def set_conert
+  def set_concert
     @concert = Concert.find(params[:id])
   end
-
 end
