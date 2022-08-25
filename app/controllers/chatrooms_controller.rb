1272e6a7155d32d6d90e13014cc795b76fa9c37c
Marcin Niemiec <marcinniemiec87@gmail.com>
2022-08-25 19:42:38 +0200

    destroy user session has been fixed

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,6 @@
 class ChatroomsController < ApplicationController
   before_action :authenticate_user!, only: %i[show]
-
+  # skip_before_action :verify_authenticity_token
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
