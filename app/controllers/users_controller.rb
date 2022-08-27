ebba419b7ef7eb6c06795c08538947bf5ca5c055
Nidhal Moussa <sinidhal2001@gmail.com>
2022-08-27 12:14:26 +0200

    add review part

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @concerts = Concert.all.order(created_at: :asc)
-
+    @username = current_user.username
+    # @reviewContent = Review.first.content
+    @reviewRating = Review.first.rating
   end
 end
