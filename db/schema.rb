2655decaaa0e059e3366f9d6bdc376d5a93efaf5
Federica Di Vincenzo <federicadivincenzo5@gmail.com>
2022-08-27 13:04:20 +0200

    added photo to each concert and user and each concert has a chatroom

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_172519) do
+ActiveRecord::Schema.define(version: 2022_08_27_095617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_08_23_172519) do
     t.string "genre"
     t.float "latitude"
     t.float "longitude"
+    t.string "photo"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -87,6 +88,7 @@ ActiveRecord::Schema.define(version: 2022_08_23_172519) do
     t.date "birthday"
     t.text "bio"
     t.string "username"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,25 +17,158 @@ Chatroom.destroy_all
 puts 'Database cleaned ✅'
 puts
 puts 'Creating users...'
-User.create!(username: 'fakeuser1', email: 'fake1@gmail.com', password: 'password')
-User.create!(username: 'fakeuser2', email: 'fake2@gmail.com', password: 'password')
-User.create!(username: 'fakeuser3', email: 'fake3@gmail.com', password: 'password')
+User.create!(username: 'fakeuser1', email: 'fake1@gmail.com', password: 'password', photo: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80')
+User.create!(username: 'fakeuser2', email: 'fake2@gmail.com', password: 'password', photo: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80')
+User.create!(username: 'fakeuser3', email: 'fake3@gmail.com', password: 'password', photo: 'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80')
 puts 'Users created ✅'
 puts
 puts 'Creating concerts...'
-10.times do
-  concert = Concert.new(
-    artist: Faker::Music.band,
-    date: Faker::Date.between(from: '2022-01-01', to: '2024-01-01'),
-    address: Faker::Address.street_address,
-    venue: Faker::Company.name,
-    description: Faker::Lorem.paragraph(sentence_count: 3),
-    rating: rand(1..5),
-    genre: Faker::Music.genre
-  )
-  concert.save!
-  puts "Concert with #{concert.artist} at #{concert.venue} created ✅"
-end
+# 10.times do
+#   concert = Concert.new(
+#     artist: Faker::Music.band,
+#     date: Faker::Date.between(from: '2022-01-01', to: '2024-01-01'),
+#     address: Faker::Address.street_address,
+#     venue: Faker::Company.name,
+#     description: Faker::Lorem.paragraph(sentence_count: 3),
+#     rating: rand(1..5),
+#     genre: Faker::Music.genre,
+#     photo:
+#   )
+#   concert.save!
+#   puts "Concert with #{concert.artist} at #{concert.venue} created ✅"
+# end
+
+concert_1 = Concert.new(
+  artist: 'Embrace',
+  date: Faker::Date.between(from: '2022-01-01', to: '2024-01-01'),
+  address: 'Aberdeen',
+  venue: 'Aberdeen Music Hall',
+  description: Faker::Lorem.paragraph(sentence_count: 3),
+  rating: rand(1..5),
+  genre: 'Rock',
+  photo: 'https://media.ticketmaster.co.uk/tm/en-gb/dam/a/10e/8f98b1c5-1fc5-450d-bc1a-92a7e124510e_1664931_CUSTOM.jpg'
+)
+concert_1.save!
+puts "Concert with #{concert_1.artist} at #{concert_1.venue} created ✅"
+
+concert_2 = Concert.new(
+  artist: 'Red Hot Chilli Peppers',
+  date: Faker::Date.between(from: '2022-01-01', to: '2024-01-01'),
+  address: 'Florence',
+  venue: 'Firenze Rock Arena',
+  description: Faker::Lorem.paragraph(sentence_count: 3),
+  rating: rand(1..5),
+  genre: 'Rock',
+  photo: 'https://media.gettyimages.com/photos/in-this-handout-image-made-available-on-march-1-2007-by-mtv-members-picture-id73490545?s=594x594'
+)
+concert_2.save!
+puts "Concert with #{concert_2.artist} at #{concert_2.venue} created ✅"
+
+concert_3 = Concert.new(
+  artist: 'Harry Styles',
+  date: Faker::Date.between(from: '2022-01-01', to: '2024-01-01'),
+  address: 'London',
+  venue: 'Wembley stadium',
+  description: Faker::Lorem.paragraph(sentence_count: 3),
+  rating: rand(1..5),
+  genre: 'Pop',
+  photo: 'https://media.gettyimages.com/photos/in-this-handout-photo-provided-by-helene-marie-pambrun-harry-styles-picture-id931619330?s=612x612'
+)
+concert_3.save!
+puts "Concert with #{concert_3.artist} at #{concert_3.venue} created ✅"
+
+concert_4 = Concert.new(
+  artist: 'The Cure',
+  date: Faker::Date.between(from: '2022-01-01', to: '2024-01-01'),
+  address: 'Milan',
+  venue: 'Assago Forum',
+  description: Faker::Lorem.paragraph(sentence_count: 3),
+  rating: rand(1..5),
+  genre: 'Rock',
+  photo: 'image.png'
+)
+concert_4.save!
+puts "Concert with #{concert_4.artist} at #{concert_4.venue} created ✅"
+
+concert_5 = Concert.new(
+  artist: 'The Cure',
+  date: Faker::Date.between(from: '2022-01-01', to: '2024-01-01'),
+  address: 'Milan',
+  venue: 'Assago Forum',
+  description: Faker::Lorem.paragraph(sentence_count: 3),
+  rating: rand(1..5),
+  genre: 'Rock',
+  photo: 'https://media.gettyimages.com/photos/the-cure-posed-studio-group-shot-circa-1984-lr-phil-thornalley-porl-picture-id85001404?s=612x612'
+)
+concert_5.save!
+puts "Concert with #{concert_5.artist} at #{concert_5.venue} created ✅"
+
+concert_6 = Concert.new(
+  artist: 'Serena Braccale',
+  date: Faker::Date.between(from: '2022-01-01', to: '2024-01-01'),
+  address: 'Roma',
+  venue: 'Auditorium Parco della Musica',
+  description: Faker::Lorem.paragraph(sentence_count: 3),
+  rating: rand(1..5),
+  genre: 'Jazz',
+  photo: 'https://www.serenabrancale.com/wp-content/uploads/2021/10/serena-brancale.jpg'
+)
+concert_6.save!
+puts "Concert with #{concert_6.artist} at #{concert_6.venue} created ✅"
+
+concert_7 = Concert.new(
+  artist: 'Sean Paul',
+  date: Faker::Date.between(from: '2022-01-01', to: '2024-01-01'),
+  address: 'London',
+  venue: 'O2 Arena',
+  description: Faker::Lorem.paragraph(sentence_count: 3),
+  rating: rand(1..5),
+  genre: 'Rap',
+  photo: 'https://pbs.twimg.com/profile_images/1085955424342425601/3pqRRO9n_400x400.jpg'
+)
+concert_7.save!
+puts "Concert with #{concert_7.artist} at #{concert_7.venue} created ✅"
+
+concert_8 = Concert.new(
+  artist: 'Snoop Dogg',
+  date: Faker::Date.between(from: '2022-01-01', to: '2024-01-01'),
+  address: 'Leeds',
+  venue: 'First direct Arena',
+  description: Faker::Lorem.paragraph(sentence_count: 3),
+  rating: rand(1..5),
+  genre: 'Rap',
+  photo: 'https://www.rollingstone.it/wp-content/uploads/2021/10/snoop-getty-ita.jpg'
+)
+concert_8.save!
+puts "Concert with #{concert_8.artist} at #{concert_8.venue} created ✅"
+
+concert_9 = Concert.new(
+  artist: 'Snoop Dogg',
+  date: Faker::Date.between(from: '2022-01-01', to: '2024-01-01'),
+  address: 'Leeds',
+  venue: 'First direct Arena',
+  description: Faker::Lorem.paragraph(sentence_count: 3),
+  rating: rand(1..5),
+  genre: 'Rap',
+  photo: 'https://www.rollingstone.it/wp-content/uploads/2021/10/snoop-getty-ita.jpg'
+)
+concert_9.save!
+puts "Concert with #{concert_9.artist} at #{concert_9.venue} created ✅"
+
+concert_10 = Concert.new(
+  artist: 'Muse',
+  date: Faker::Date.between(from: '2022-01-01', to: '2024-01-01'),
+  address: 'Glasgow',
+  venue: 'Bellahouston Park',
+  description: Faker::Lorem.paragraph(sentence_count: 3),
+  rating: rand(1..5),
+  genre: 'Rock',
+  photo: 'https://media.gettyimages.com/photos/british-rock-band-muse-composed-by-bassist-christopher-wolstenholme-picture-id1060029064?s=612x612'
+)
+concert_10.save!
+puts "Concert with #{concert_9.artist} at #{concert_9.venue} created ✅"
+
+
 puts '===================='
 puts 'Concerts created ✅'
 puts
@@ -51,11 +184,13 @@ Review.create!(
 puts 'Review created ✅'
 puts
 puts 'Creating chatroom...'
-Chatroom.create!(name: '#test', concert_id: Concert.all.sample.id)
+Concert.all.each do |concert|
+  Chatroom.create!(name: "test#{rand(1..10)}", concert_id: concert.id)
+end
 puts 'Chatroom created ✅'
 puts 'Creating messages...'
-3.times do
-  Message.create!(content: Faker::Company.bs, user_id: User.all.sample.id, chatroom_id: Chatroom.first.id)
+Chatroom.all.each do |chatroom|
+  Message.create!(content: Faker::Company.bs, user_id: User.all.sample.id, chatroom_id: chatroom.id)
 end
 puts 'Messages created ✅'
 puts
