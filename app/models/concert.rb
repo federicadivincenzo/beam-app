f7443ee5d9c2c2ee0f85711de32340817c4493c2
Federica Di Vincenzo <federicadivincenzo5@gmail.com>
2022-08-31 16:03:43 +0200

    tabs filter by category

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -5,8 +5,8 @@ class Concert < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   include PgSearch::Model
-  pg_search_scope :search_by_artist_address_venue_and_description,
-    against: [ :artist, :address, :venue, :description ],
+  pg_search_scope :search_by_artist_address_venue_genre_description,
+    against: [ :artist, :address, :venue, :description, :genre ],
     using: {
       tsearch: { prefix: true }
     }
