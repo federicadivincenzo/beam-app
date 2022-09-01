a7d8733491e250cd969f86a5174a5dc199a42e27
Nidhal Moussa <sinidhal2001@gmail.com>
2022-09-01 21:28:38 +0200

    add review page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "review", to: "reviews#home"
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :concerts, only: %i[index show] do
