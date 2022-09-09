69057e5e0e7613bfa02bc7fd5466ebd0d2351ad0
Marcin Niemiec <marcinniemiec87@gmail.com>
2022-09-09 21:17:36 +0200

    Fixed bugs. Droplist, attending list and map is working.

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -10,5 +10,6 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+    @messages = Message.where(chatroom_id: params[:id])
   end
 end
