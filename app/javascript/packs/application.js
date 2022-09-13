2256ea36cb50198217f3d21079539b162dd0ebbb
Federica Di Vincenzo <federicadivincenzo5@gmail.com>
2022-09-13 12:26:40 +0200

    footer and some animation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,9 +17,29 @@ ActiveStorage.start()
 
 var animateScaling = anime({
   targets: '.scale-animation',
-  scale: [
-    { value: .75},
-    { value: 1 },
-  ],
+  scale: [14,1],
+  opacity: [0,1],
+  easing: "easeOutCirc",
+  duration: 800,
   autoplay: true
+});
+
+
+var animateScalingLogo = anime({
+  targets: '#logo-white',
+  scale: [14,1],
+  opacity: [0,1],
+  easing: "easeOutCirc",
+  duration: 800,
+  autoplay: true
+});
+
+var animateScalingImage = anime({
+  targets: '.card-value-img',
+  translateY: [100,0],
+  translateZ: 0,
+  opacity: [0,1],
+  easing: "easeOutExpo",
+  duration: 3000,
+  delay: (el, i) => 300 + 30 * i
 });
